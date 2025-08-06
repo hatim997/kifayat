b9b93e70f7ffe6f8d36176795aa3cf5dd04b71d9
hatim997 <laravel.supersoft@gmail.com>
2025-08-06 18:17:51 +0500

    route changes issue fixed

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -3,24 +3,37 @@
     {{-- <img src="{{ asset('assets/images/account/profile.png') }}" alt="User" class="avatar"> --}}
 
     <!-- Avatar Trigger -->
-    <img src="{{ asset('assets/images/account/profile.png') }}" alt="User" class="avatar"
-         data-bs-toggle="offcanvas" data-bs-target="#sidebarOffcanvas" aria-controls="sidebarOffcanvas">
+    {{-- <img src="{{ asset('assets/images/account/profile.png') }}" alt="User" class="avatar" data-bs-toggle="offcanvas"
+        data-bs-target="#sidebarOffcanvas" aria-controls="sidebarOffcanvas"> --}}
+    @php
+        $name = Auth::user()->name;
+        $initials = collect(explode(' ', $name))->map(fn($part) => strtoupper($part[0]))->take(2)->implode('');
+        $colors = ['#F87171', '#60A5FA', '#34D399', '#FBBF24', '#A78BFA', '#F472B6', '#38BDF8'];
+        $color = $colors[crc32($name) % count($colors)];
+    @endphp
+
+    <div class="user-initials-avatar" style="background-color: {{ $color }};" data-bs-toggle="offcanvas"
+        data-bs-target="#sidebarOffcanvas" aria-controls="sidebarOffcanvas">
+        {{ $initials }}
+    </div>
 </div>
 
 <!-- Offcanvas Sidebar -->
-<div class="offcanvas offcanvas-end custom-sidebar" tabindex="-1" id="sidebarOffcanvas" aria-labelledby="sidebarOffcanvasLabel">
-  <div class="offcanvas-header border-bottom">
-    <h5 class="offcanvas-title" id="sidebarOffcanvasLabel">{{ Auth::user()->name }}</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body d-flex flex-column justify-content-between">
-    <div>
-        <a href="#" class="sidebar-link"><i class="fas fa-user me-2"></i> Profile</a>
-        <a href="#" class="sidebar-link"><i class="fas fa-cog me-2"></i> Settings</a>
+<div class="offcanvas offcanvas-end custom-sidebar" tabindex="-1" id="sidebarOffcanvas"
+    aria-labelledby="sidebarOffcanvasLabel">
+    <div class="offcanvas-header border-bottom">
+        <h5 class="offcanvas-title" id="sidebarOffcanvasLabel">{{ Auth::user()->name }}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <button class="btn btn-logout w-100 mt-4" type="submit"><i class="fas fa-sign-out-alt me-2"></i> Logout</button>
-    </form>
-  </div>
+    <div class="offcanvas-body d-flex flex-column justify-content-between">
+        <div>
+            <a href="#" class="sidebar-link"><i class="fas fa-user me-2"></i> Profile</a>
+            <a href="#" class="sidebar-link"><i class="fas fa-cog me-2"></i> Settings</a>
+        </div>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button class="btn btn-logout w-100 mt-4" type="submit"><i class="fas fa-sign-out-alt me-2"></i>
+                Logout</button>
+        </form>
+    </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
+// Auth::routes();
 Route::get('/', function () {
     return view('frontend.pages.choose-portal');
 })->name('choose-portal');
